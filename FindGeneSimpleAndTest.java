a00337e8abc2cc9b8494608f0eeacfc02c940ed4
bruno78 <devbruno78@gmail.com>
2017-05-10 14:05:42 -0400

    Add findgene method

--- FindGeneSimpleAndTest.java
+++ FindGeneSimpleAndTest.java
@@ -8,5 +8,14 @@
 public class FindGeneSimpleAndTest
 {
     public String findGeneSimple(String dna ) {
+        // start codon is "ATG"
+        // stop codon is "TAA"
+        
+        String result = "";
+        int startIndex = dna.indexOf("ATG");
+        int stopIndex = dna.indexOf("TAA", startIndex+3);
+        result = dna.substring(startIndex, stopIndex+3);
+        
+        return result;
     }
 }
