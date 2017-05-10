c9cbd23fef29dd7185bc0439c075b8e7f2385f1c
bruno78 <devbruno78@gmail.com>
2017-05-10 14:21:12 -0400

    Add error handler

--- FindGeneSimpleAndTest.java
+++ FindGeneSimpleAndTest.java
@@ -14,8 +14,11 @@ public class FindGeneSimpleAndTest
         String result = "";
         int startIndex = dna.indexOf("ATG");
         int stopIndex = dna.indexOf("TAA", startIndex+3);
+         if (startIndex == -1 || stopIndex == -1) {
+            return "not found here";
+        }
         result = dna.substring(startIndex, stopIndex+3);
-        
+       
         return result;
     }
     
@@ -31,6 +34,18 @@ public class FindGeneSimpleAndTest
         System.out.println("Gene is " + gene);
         
         dna = "ATCCTATGCTTCGGCTGCTCTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is " + gene);
+        
+        // No start gene case 
+        dna = "TTATAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is " + gene);
+        
+        
+         dna = "TATGTATATCG";
         System.out.println("DNA strand is " + dna);
         gene = findGeneSimple(dna);
         System.out.println("Gene is " + gene);
