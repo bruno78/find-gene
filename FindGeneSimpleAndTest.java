bc8ef33ae437ad04634fd1a0336adc25bf6f963d
bruno78 <devbruno78@gmail.com>
2017-05-10 13:59:35 -0400

    Initial commit

---
+++ FindGeneSimpleAndTest.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class FindGeneSimpleAndTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FindGeneSimpleAndTest
+{
+    public String findGeneSimple(String dna ) {
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple program to find gene in a genomic strand 
+
+In a genomic strand, it looks for the start codon ATG and the stop codon TAA, and it includes all characters between those two, which forms a gene. 
