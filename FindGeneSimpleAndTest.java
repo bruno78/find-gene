2c2db2277aa653c9eecb52a445755fb89f2d47eb
bruno78 <devbruno78@gmail.com>
2017-05-12 15:52:58 -0400

    Refactor

--- FindGeneSimpleAndTest.java
+++ FindGeneSimpleAndTest.java
@@ -7,21 +7,7 @@
  */
 public class FindGeneSimpleAndTest
 {
-    public String findGeneSimple(String dna ) {
-        // start codon is "ATG"
-        // stop codon is "TAA"
-        
-        String result = "";
-        int startIndex = dna.indexOf("ATG");
-        int stopIndex = dna.indexOf("TAA", startIndex+3);
-         if (startIndex == -1 || stopIndex == -1) {
-            return "not found here";
-        }
-        
-        result = dna.substring(startIndex, stopIndex+3);
-       
-        return result;
-    }
+
     
     public String findGene(String dna) {
         int startIndex = dna.indexOf("ATG");
@@ -38,31 +24,10 @@ public class FindGeneSimpleAndTest
     }
     
     public void testfindGeneSimple() {
-        String dna = "AATGCGTAATATGGT";
-        System.out.println("DNA strand is " + dna);
-        String gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
         
-        dna = "AATGCTAGGGTAATATGGT";
+        String dna = "TATGTATATCG";
         System.out.println("DNA strand is " + dna);
-        gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
-        
-        dna = "ATCCTATGCTTCGGCTGCTCTAATATGGT";
-        System.out.println("DNA strand is " + dna);
-        gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
-        
-        // No start gene case 
-        dna = "TTATAA";
-        System.out.println("DNA strand is " + dna);
-        gene = findGeneSimple(dna);
-        System.out.println("Gene is " + gene);
-        
-        
-        dna = "TATGTATATCG";
-        System.out.println("DNA strand is " + dna);
-        gene = findGene(dna);
+        String gene = findGene(dna);
         System.out.println("Gene is " + gene);
         
         dna = "AATGCTAGGGTAATATGGT";
